b78387692476c89bafdc8639779290025d5b345c
yoyobara <yotambarak6@gmail.com>
2024-04-01 16:41:37 +0300

    edges indexes

--- include/cube.h
+++ include/cube.h
@@ -7,6 +7,21 @@ typedef struct cube {
     int offset[2];
 } cube;
 
+const int EDGES_INDEXES[12][2] = {
+        {0, 1},
+        {0, 2},
+        {0, 4},
+        {1, 3},
+        {1, 5},
+        {2, 3},
+        {2, 6},
+        {3, 7},
+        {4, 5},
+        {4, 6},
+        {5, 7},
+        {6, 7}
+};
+
 // creates a new cube
 cube cube_new(unsigned int size, int x_offset, int y_offset);
 
