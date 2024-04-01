a72ac948f88176fca6f26d905281a5ca833193b2
yoyobara <yotambarak6@gmail.com>
2024-04-01 03:05:27 +0300

    cube size

--- include/cube.h
+++ include/cube.h
@@ -1,6 +1,8 @@
 #ifndef CUBE_H
 #define CUBE_H
 
+#define CUBE_SIZE 10
+
 // a struct representing a 3D cube
 typedef struct cube {
     int vertices[8][3];
--- src/cube.c
+++ src/cube.c
@@ -6,9 +6,9 @@ cube cube_new() {
 
     // fill vertices
     int index_counter = 0;
-    for (int i = -1 ; i != 3 ; i += 2) 
-        for (int j = -1 ; j != 3 ; j += 2)
-            for (int k = -1 ; k != 3 ; k += 2) {
+    for (int i = -CUBE_SIZE ; i != 3 * CUBE_SIZE ; i += 2 * CUBE_SIZE)
+        for (int j = -CUBE_SIZE ; j != 3 * CUBE_SIZE ; j += 2 * CUBE_SIZE)
+            for (int k = -CUBE_SIZE ; k != 3 * CUBE_SIZE ; k += 2 * CUBE_SIZE) {
                 c.vertices[index_counter][0] = i;
                 c.vertices[index_counter][1] = j;
                 c.vertices[index_counter][2] = k;
